90c81863efe7e0d1e3f3138085b05f2a557aaa50
kaichun323 <houk4077@gmail.com>
2023-04-08 11:33:29 +0800

    更新內容

---
+++ w43.js
@@ -0,0 +1,11 @@
+var menu = document.getElementById("menu");
+function open() {
+    menu.style.right = "0";
+    console.log("open");
+}
+function close() {
+    menu.style.right = "-300px";
+    console.log("close");
+}
+console.log(menu);
+console.log("menu");
